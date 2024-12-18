3484f2792ddea9fd489ca2d35dba5c757f8b842b
Kyle Vu <ktvu@hbuhsd.edu>
2024-12-18 12:16:29 -0800

    Update classwork.cpp

--- classwork.cpp
+++ classwork.cpp
@@ -1,11 +1,9 @@
-#include <cmath>     // std::abs, std::sqrt, std::pow
-#include <cstdlib>   // std::srand, std::rand
-#include <ctime>     // std::time
 #include <iostream>  // std::cin, std::cout, std::getline
 #include <string>    // std::string
 
 int main()
 {
-
-  return 0;
+    // You can follow along with any code in class here
+    
+    return 0;
 }
